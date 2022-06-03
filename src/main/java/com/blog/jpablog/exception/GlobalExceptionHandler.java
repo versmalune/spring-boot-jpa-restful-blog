06d4d9381bd9ce105ef178c572261f085b8713f3
versmalune <versmalune@gmail.com>
2022-06-03 18:05:38 +0900

    Global Exception Handler

--- src/main/java/com/blog/jpablog/exception/GlobalExceptionHandler.java
+++ src/main/java/com/blog/jpablog/exception/GlobalExceptionHandler.java
@@ -1,4 +1,33 @@
 package com.blog.jpablog.exception;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
+    @ExceptionHandler(CustomException.class) // CUSTOM EXCEPTION
+    protected ResponseEntity<ErrorResponse> handleCustomException(final CustomException e) {
+        log.error("handleCustomException: {}", e.getErrorCode());
+        return ResponseEntity
+                .status(e.getErrorCode().getStatus().value())
+                .body(new ErrorResponse(e.getErrorCode()));
+    }
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class) // 405 EXCEPTION
+    protected ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(final HttpRequestMethodNotSupportedException e) {
+        log.error("handleHttpRequestMethodNotSupportedException: {}", e.getMessage());
+        return ResponseEntity
+                .status(ErrorCode.METHOD_NOT_ALLOWED.getStatus().value())
+                .body(new ErrorResponse(ErrorCode.METHOD_NOT_ALLOWED));
+    }
+    @ExceptionHandler(Exception.class) // 500 EXCEPTION
+    protected ResponseEntity<ErrorResponse> handleException(final Exception e) {
+        log.error("handleException: {}", e.getMessage());
+        return ResponseEntity
+                .status(ErrorCode.INTERNAL_SERVER_ERROR.getStatus().value())
+                .body(new ErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR));
+    }
 }
